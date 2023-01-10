2ffba0e0e73ed4ca84e8a536a78d9b1730071afe
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-10 10:55:33 -0500

    Evento mostrar agregado

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class DataBindingComponent {
   titulo: string = 'Data Binding Interpolation '
+  texto!: string
+  valueButton: string = 'Show'
+  mostrarTexto() {
+    this.texto = 'Event Binding, es el mecanismo de data binding mediante el cual trabaja con los eventos que se encuentra en el DOM'
+    this.valueButton = "Hide"
+  }
+
   twoWay!: string
 }
